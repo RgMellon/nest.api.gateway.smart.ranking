642d007866dbc3ae0c123c7e5c152f74beeaa91b
RgMellon <renan_gian@hotmail.com>
2024-09-10 20:46:35 -0300

    chore: add config setup

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
+import { TimeoutInterceptor } from './interceptor/timeout.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+  await app.listen(3000);
+}
+bootstrap();
